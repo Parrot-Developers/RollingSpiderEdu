9bcf460b0f6c853d56879d123cdef257709d12e2
friether <fr@engineering-sciences.de>
2015-09-28 18:13:31 -0500

    fix computer's IP address with using ad-hoc IP

--- MIT_MatlabToolbox/bin/DroneInitialize.sh
+++ MIT_MatlabToolbox/bin/DroneInitialize.sh
@@ -5,9 +5,9 @@ cd `dirname $0`
 
 # 2. Findout this computer's IP
 myip="$(ifconfig eth1 2>/dev/null|awk '/inet addr:/ {print $2}'|sed 's/addr://')"
-echo "> Drone: This computer's IP is: $myip."
+#echo "> Drone: This computer's IP is: $myip."
 
 # 3. Telnet into drone, and set ad-hoc network IP variable in param file
-echo "> Drone: Uploading the IP parameter into the drone."
+echo "> Drone: Uploading the ad-hoc IP 192.168.1.2 parameter into the drone."
 /usr/bin/expect -c 'set timeout 10; spawn telnet 192.168.1.1; expect "RS.edu] \$ "; send "cd /data/edu/params; sed \"1s/.*/IP : 192.168.1.2;/\" paramsEDU.dat > paramsEDU.dat.bc; mv paramsEDU.dat.bc paramsEDU.dat; exit\r"; expect eof'
 
